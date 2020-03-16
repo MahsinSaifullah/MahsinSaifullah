72ecee3fe6cb370a1a61c8684b23d3a0b88acfc8
macbookpro <mahsinsaifullah@gmail.com>
2020-03-16 14:25:38 +0000

    initial commit

---
+++ src/Palette.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Palette extends Component {
+	state = {};
+	render() {
+		return (
+			<div>
+				{/* Navbar */}
+				<div>{/* Color Box */}</div>
+				{/* Footer */}
+			</div>
+		);
+	}
+}
+
+export default Palette;
