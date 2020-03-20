8af5f9964114591c7dc07151f3c27b2f1a7e03e8
macbookpro <mahsinsaifullah@gmail.com>
2020-03-20 21:31:11 +0000

    Add NewPalette Form and Link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Switch } from 'react-router-dom';
 import { generatePalette } from './colorHelpers';
 import PaletteList from './PaletteList';
 import SingleColorPalette from './SingleColorPalette';
+import NewPaletteForm from './NewPaletteForm';
 
 function findPalette(id) {
 	return seedColors.find(palette => palette.id === id);
@@ -13,6 +14,7 @@ function findPalette(id) {
 function App() {
 	return (
 		<Switch>
+			<Route exact path='/palette/new' render={() => <NewPaletteForm />} />
 			<Route
 				exact
 				path='/'
