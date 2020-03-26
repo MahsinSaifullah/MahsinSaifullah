246e95ba2d839afa99cca94a2c310f00be6c8e24
macbookpro <mahsinsaifullah@gmail.com>
2020-03-26 12:02:46 +0000

    Make PaletteList responsive

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,3 +1,4 @@
+import sizes from './sizes';
 export default {
 	root: {
 		backgroundColor: 'purple',
@@ -11,7 +12,22 @@ export default {
 		display: 'flex',
 		alignItems: 'flex-start',
 		flexDirection: 'column',
-		flexWrap: 'wrap'
+		flexWrap: 'wrap',
+		[sizes.down('xl')]: {
+			width: '65%'
+		},
+		[sizes.down('lg')]: {
+			width: '70%'
+		},
+		[sizes.down('md')]: {
+			width: '60%'
+		},
+		[sizes.down('sm')]: {
+			width: '70%'
+		},
+		[sizes.down('xs')]: {
+			width: '50%'
+		}
 	},
 	nav: {
 		display: 'flex',
@@ -28,6 +44,14 @@ export default {
 		width: '100%',
 		display: 'grid',
 		gridTemplateColumns: 'repeat(3, 30%)',
-		gridGap: '5%'
+		gridGap: '5%',
+		[sizes.down('md')]: {
+			gridTemplateColumns: 'repeat(2, 50%)',
+			gridGap: '4%'
+		},
+		[sizes.down('xs')]: {
+			gridTemplateColumns: 'repeat(1, 100%)',
+			gridGap: '2%'
+		}
 	}
 };
